f0e6401ee4eca7af60ac1d79c4a89ff4af563bf2
Matt Lintlop <mattlintlop@matts-macbook-pro.local>
2018-09-22 15:53:36 -0700

    Initial Commit

---
+++ CodeChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeChallenge
+//
+//  Created by Matt Lintlop on 9/22/18.
+//  Copyright © 2018 Matt Lintlop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
