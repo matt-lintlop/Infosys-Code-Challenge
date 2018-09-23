ba68950a1befe88976c62480ee25089f04103433
Matt Lintlop <mattlintlop@matts-macbook-pro.local>
2018-09-22 19:50:18 -0700

    Started to implement the ObjectDescreption class.

---
+++ CodeChallenge/DataParser.swift
@@ -0,0 +1,13 @@
+//
+//  DataParser.swift
+//  CodeChallenge
+//
+//  Created by Matt Lintlop on 9/22/18.
+//  Copyright © 2018 Matt Lintlop. All rights reserved.
+//
+
+import Foundation
+
+func parseData(completion: (Error, [ObjectDescreption])) {
+    
+}
