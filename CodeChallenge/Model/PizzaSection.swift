d92a054368bd8b4db283138cc3649e7996652761
Matt Lintlop <matt_lintlop@icloud.com>
2019-01-28 04:11:50 -0800

    Implemented parsing of PizzaSection.

--- CodeChallenge/Model/PizzaSection.swift
+++ CodeChallenge/Model/PizzaSection.swift
@@ -11,13 +11,21 @@
 import Foundation
 
 struct PizzaSection {
-    let sectionsDict:[String:[Pizza]]      // pizza section dict (key = section name, value = pizzas in the section)
+    let sectionName:String
+    let sectionPizzas:[Pizza]?
+    
+    let sectionsDict:[String:[[String:AnyObject]]]      // pizza section dict (key = section name, value = pizzas in the section)
 
     private enum DictionaryKeys: String {
         case sectionsDict = ""
     }
     
-    init?(withDictionary dict:[String:[String:AnyObject]]) {
-        self.sectionsDict = [:]
+    init?(withDictionary dict:[String:[[String:AnyObject]]]) {
+        guard let sectionName = dict.keys.first else {
+            return nil
+        }
+        self.sectionsDict = dict
+        self.sectionName = sectionName
+        self.sectionPizzas = nil
     }
 }
