1d8962d301ac0036001e30a1fdb62c96b69ede61
Matt Lintlop <matt_lintlop@icloud.com>
2019-01-28 08:54:50 -0800

    Implemented Classification toString()

--- CodeChallenge/Model/PizzaClassifications.swift
+++ CodeChallenge/Model/PizzaClassifications.swift
@@ -11,9 +11,13 @@ import Foundation
 
 struct PizzaClassifications {
     let vegetarian:Bool         // vegetariona pizza flag
- 
+    let spicy:Bool              // vegetariona pizza flag
+    let glutonFree:Bool         // vegetariona pizza flag
+
     private enum DictionaryKeys: String {
         case vegetarian
+        case spicy
+        case glutonFree = "gluten_free"
     }
 
     init?(withDictionary dict:[String:AnyObject]) {
@@ -21,5 +25,39 @@ struct PizzaClassifications {
             return nil
         }
         self.vegetarian = vegetarian
+        
+        guard let spicy = dict[DictionaryKeys.spicy.rawValue] as? Bool else {
+            return nil
+        }
+        self.spicy = spicy
+        
+        guard let glutonFree = dict[DictionaryKeys.glutonFree.rawValue] as? Bool else {
+            return nil
+        }
+        self.glutonFree = glutonFree
+
+   }
+    
+    
+    // Get the text for this classification as a string
+    func toString() -> String? {
+        var string = ""
+        
+        if (self.vegetarian) {
+            string = "Vegetarian"
+        }
+        else if (self.spicy) {
+            if !string.isEmpty {
+                string += ", "
+            }
+            string += "Spicy"
+        }
+        else if (self.glutonFree) {
+            if !string.isEmpty {
+                string += ", "
+            }
+            string += "Gluton Free"
+        }
+        return string
     }
 }
