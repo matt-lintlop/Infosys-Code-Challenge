6bfc01d1d570b1b1bc485b1e235d4156f9ab9521
Matt Lintlop <matt_lintlop@icloud.com>
2019-01-28 04:36:20 -0800

    Using DictionaryKets dictionary in model init(withDictionary:)

--- CodeChallenge/Model/PizzaTopping.swift
+++ CodeChallenge/Model/PizzaTopping.swift
@@ -16,17 +16,24 @@ struct PizzaTopping  {
     var description:String?         // description of topping
     var assets:PizzaAssets?         // asset with url of topping image
     
+    private enum DictionaryKeys: String {
+        case id
+        case name
+        case description
+        case assets
+    }
+    
     init?(withDictionary dict:[String:AnyObject]) {
-        guard let id = dict["id"] as? Int else {
+        guard let id = dict[DictionaryKeys.id.rawValue] as? Int else {
             return nil
         }
         self.id = id
-        guard let name = dict["name"] as? String else {
+        guard let name = dict[DictionaryKeys.name.rawValue] as? String else {
             return nil
         }
         self.name = name
-        self.description = dict["description"] as? String
-        if let assetsDict = dict["assets"] as? [String:AnyObject] {
+        self.description = dict[DictionaryKeys.description.rawValue] as? String
+        if let assetsDict = dict[DictionaryKeys.assets.rawValue] as? [String:AnyObject] {
             guard let assets = PizzaAssets(withDictionary: assetsDict) else {
                 return nil
             }
